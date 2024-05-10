7b8fc1e3eca36050c6335fd3a4b30ed16778f2b5
uthman54 <uthmanbh54@gmail.com>
2024-05-10 10:53:38 +0200

    Merge remote-tracking branch 'origin/main'

--- Abstract/UtilityVehicle.java
+++ Abstract/UtilityVehicle.java
@@ -16,6 +16,6 @@ public class UtilityVehicle extends Car{
     }
 
     @Override
-    public String getInfo() {return "VIN: " + this.getVinNumber() + ", Make: " + this.getMake() + ", Model: " + this.getModel() + ", Mileage: " + this.getMileage() + ", Four Wheel Drive: " + this.getFourWheelDrive;
+    public String getInfo() {return "VIN: " + this.getVinNumber() + ", Make: " + this.getMake() + ", Model: " + this.getModel() + ", Mileage: " + this.getMileage() + ", Four Wheel Drive: " + this.getfourWheelDrive;
     }
 }
